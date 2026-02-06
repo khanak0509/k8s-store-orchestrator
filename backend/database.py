1c0f11b5b32b44279cc0b75bef3970d45d26371c
khanak0509 <kkhanak512@gmail.com>
2026-02-07 03:22:18 +0530

    expose for all database

--- backend/database.py
+++ backend/database.py
@@ -7,7 +7,14 @@ from sqlalchemy.orm import Session
 
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./sql_app.db")
 
+connect_args = {}
+if SQLALCHEMY_DATABASE_URL.startswith("sqlite"):
+    connect_args = {"check_same_thread": False}
 
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, 
+    connect_args=connect_args
+)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
